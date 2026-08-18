22034f602617215fcb08ba76bdd89b105b7c0c2c
curciog <curciogdev@gmail.com>
2026-08-18 17:09:05 -0300

    refactor: change controller method

--- src/main/java/com/gabriel/booksapi/controller/BooksController.java
+++ src/main/java/com/gabriel/booksapi/controller/BooksController.java
@@ -1,20 +1,23 @@
 package com.gabriel.booksapi.controller;
 
+import com.gabriel.booksapi.entity.Book;
+import com.gabriel.booksapi.service.BookService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/books")
+@RequiredArgsConstructor
 public class BooksController {
 
+    private final BookService bookService;
+
     @GetMapping
-    public String getBooks() {
-        return "Books API is working.";
+    public List<Book> findAll() {
+        return bookService.findAll();
     }
-
-    /*@GetMapping("/{id}")
-    public String getBooksById(long id) {
-
-    }*/
 }
